bf92d1f46ace4eecb48d402a7ceefd2d68490814
Nathan LeSueur <nathanle@akamai.com>
2025-10-23 09:33:49 -0400

    30day

--- akamai_billing.py
+++ akamai_billing.py
@@ -6,6 +6,7 @@ import json
 import sys
 import os
 import argparse
+from datetime import datetime
 import pandas as pd 
 
 import requests
@@ -22,5 +23,9 @@ url = "https://api.linode.com/{}/account/invoices".format(apiversion)
 response = requests.get(url, headers=headers)
 data = response.json()
 df = pd.DataFrame(data['data'])
+df['date'] = pd.to_datetime(df['date'])
+cutoff_date = datetime.now() - pd.Timedelta(days=30)
 
 print(df)
+records_last_30_days = df[df['date'] > cutoff_date]
+print(records_last_30_days)
